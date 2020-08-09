1e7c998c843ad290e52b5d60d97a250a8aa14d18
Emanuel Massafera <emanuel301@live.com>
2020-08-09 12:04:16 -0300

    Mensagem de erro e botão de remover horários

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,13 +1,52 @@
 document.querySelector("#add-time").addEventListener("click", cloneField);
 
 function cloneField() {
-    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+    const containers = document.querySelectorAll(".schedule-item");
+    const lastContainer = containers[containers.length - 1];
+    let cont = 0;
 
-    const fields = newFieldContainer.querySelectorAll("input");
+    const lastContainerInputs = lastContainer.querySelectorAll("input");
+    const lastContainerSelect = lastContainer.querySelector("select");
 
-    fields.forEach(field => {
-        field.value = "";
+    lastContainerInputs.forEach(field => {
+        if (field.value == "") {
+            cont += 1;
+        }
     });
 
-    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+    if (lastContainerSelect.value == "") {
+        cont += 1;
+    }
+
+    if (cont == 0) {
+        if (containers.length - 1 == 0) {
+            lastContainer.querySelector(".alert-and-close").classList.remove("active");
+        }
+        
+        lastContainer.querySelector(".alert").classList.remove("active");           
+
+        const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+
+        const fields = newFieldContainer.querySelectorAll("input");
+
+        fields.forEach(field => {
+            field.value = "";
+        });
+
+        newFieldContainer.querySelector(".alert-and-close").classList.add("active");
+        newFieldContainer.querySelector(".close").classList.add("active");
+        newFieldContainer.querySelector(".close").addEventListener("click", function() {
+            const parentNode = newFieldContainer.querySelector(".close").parentNode;
+            const parentField = parentNode.parentNode;
+            const parentContainer = parentField.parentNode;
+
+            parentContainer.removeChild(parentField);
+        });
+
+        document.querySelector("#schedule-items").appendChild(newFieldContainer);
+
+    } else {
+        lastContainer.querySelector(".alert-and-close").classList.add("active");
+        lastContainer.querySelector(".alert").classList.add("active");
+    }
 }
